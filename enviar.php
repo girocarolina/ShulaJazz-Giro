ddf18f2a7824b29904f40f01d9965a88ee04609a
Carolina Giro <girocarolina@gmail.com>
2021-07-13 20:58:07 -0300

    Arreglo del formulario contact-form

--- enviar.php
+++ enviar.php
@@ -1,24 +1,23 @@
 <?php
-$nombre = $_POST['nombre']
-$email = $_POST['email']
-$phone = $_POST['telefono']
-$mensaje = $_POST['mensaje']
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 
-$header = 'From: ' . $email . " \r\n";
+$header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
-​
-$message = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$message .= "Su e-mail es: " . $email . " \r\n";
-$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$message .= "Enviado el " . date('d/m/Y', time());
-​
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
 $para = 'girocarolina@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
-​
-mail($para, $asunto, utf8_decode($message), $header);
-​
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
 header('Location:index.html');
 
 ?>
